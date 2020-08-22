f00dcae91fee972d53ae0a693ae9a1f150fe544c
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-08-22 16:07:22 +0530

    Added components and image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import Home from './Home'
-import About  from './About'
-import Service  from './Service'
-import Contact from './Contact'
+import About  from '../src/components/About'
+import Service  from '../src/components/Service'
+import Contact from '../src/components/Contact'
 import { Switch,Route} from 'react-router-dom';
+import Navbar from '../src/components/Navbar'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 
 const App = () =>{
 
   return (
     <>
+    <Navbar />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/service" component={Service} />
-        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/service" component={Service} ></Route>
+        <Route exact path="/contact" component={Contact} ></Route>
       </Switch>
    </>
   );
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
-
-
+import img from '../src/img/weather.jpg';
 const Home = () =>{
-
 return(
     <>
-    <h1>Home page</h1>
+    <section id="header" className='d-flex align-item-center'>
+    <div className="container-fluid nav_bg">
+        <div className="row">
+             <div className="col-10 mx-auto">
+             <div className="row">
+               <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex align-item-center justify-content-center flex-column">
+                  <h1>
+                    Grow Your Business with <strong className="brand-name"> Yogesh Gaur</strong> 
+                  </h1>
+
+                  <h2 className='my-3'>
+                    We are the team of talented developer making website
+                  </h2>
+                  <div className="mt-3">
+                  <a href="" className='btn btn-outline-primary'> Get Started
+                  </a>
+                  </div>
+                </div>
+                <div className="col-lg-6 order-1 order-lg-2 header-img">
+                   <img src={img} alt="home.img"/>
+                </div>
+                </div>
+             </div>
+        </div>
+    </div>
+    </section>
    </>
 );
-};
+}
 
 export default Home;
